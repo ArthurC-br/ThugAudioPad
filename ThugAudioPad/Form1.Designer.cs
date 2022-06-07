a814df09ff107c64ebeb8f1a856ce0f92dc3a390
HashBat <64814564+ArthurChavaum@users.noreply.github.com>
2022-06-07 10:59:05 -0300

    Add files via upload

---
+++ ThugAudioPad/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace ThugAudioPad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.CB_list_devices = new System.Windows.Forms.ComboBox();
+            this.listView_mp3Files = new System.Windows.Forms.ListView();
+            this.mp3_files = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.comandos = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CB_list_devices
+            // 
+            this.CB_list_devices.FormattingEnabled = true;
+            this.CB_list_devices.Location = new System.Drawing.Point(572, 12);
+            this.CB_list_devices.Name = "CB_list_devices";
+            this.CB_list_devices.Size = new System.Drawing.Size(216, 21);
+            this.CB_list_devices.TabIndex = 2;
+            // 
+            // listView_mp3Files
+            // 
+            this.listView_mp3Files.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.mp3_files,
+            this.comandos});
+            this.listView_mp3Files.HideSelection = false;
+            this.listView_mp3Files.Location = new System.Drawing.Point(12, 32);
+            this.listView_mp3Files.Name = "listView_mp3Files";
+            this.listView_mp3Files.Size = new System.Drawing.Size(554, 228);
+            this.listView_mp3Files.TabIndex = 4;
+            this.listView_mp3Files.UseCompatibleStateImageBehavior = false;
+            this.listView_mp3Files.View = System.Windows.Forms.View.List;
+            // 
+            // mp3_files
+            // 
+            this.mp3_files.Width = 400;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Selecione seu audio:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ThugAudioPad.Properties.Resources.toppng_com_thug_life_text_logo_big_1500x1500;
+            this.pictureBox1.Location = new System.Drawing.Point(572, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(216, 221);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 272);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView_mp3Files);
+            this.Controls.Add(this.CB_list_devices);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = " ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox CB_list_devices;
+        private System.Windows.Forms.ListView listView_mp3Files;
+        private System.Windows.Forms.ColumnHeader mp3_files;
+        private System.Windows.Forms.ColumnHeader comandos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ThugAudioPad/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System.IO;
+using KeyDownTester.Keys;
+using System.Windows.Input;
+
+namespace ThugAudioPad
+{
+
+    public partial class Form1 : Form
+    {
+        private WaveOutEvent outputDevice;
+        private AudioFileReader audioFile;
+        private WaveOutEvent MyOutputDevice;
+        private void list_device_audio()
+        {
+            for(int n=-1; n<WaveOut.DeviceCount; n++)
+            {
+                var cap = WaveOut.GetCapabilities(n);
+                CB_list_devices.Items.Add(cap.ProductName);
+                if (cap.ProductName.Contains("VB-Audio"))
+                {
+                    CB_list_devices.SelectedIndex = n+1;
+                }
+
+            }
+        }
+        private void list_mp3_files()
+        {
+            var files = Directory.GetFiles(@"C:\Users\sons");
+            foreach(string file in files)
+            {
+                listView_mp3Files.Items.Add(file);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            HotkeysManager.SetupSystemHook();
+            HotkeysManager.AddHotkey(System.Windows.Input.ModifierKeys.Control, Key.NumPad0, () => { stop(); });
+            HotkeysManager.AddHotkey(System.Windows.Input.ModifierKeys.Control, Key.NumPad1, () => { Play_Sound(listView_mp3Files.Items[1].Text.ToString()); });
+            HotkeysManager.AddHotkey(System.Windows.Input.ModifierKeys.Control, Key.NumPad2, () => { Play_Sound(listView_mp3Files.Items[2].Text.ToString()); });
+            HotkeysManager.AddHotkey(System.Windows.Input.ModifierKeys.Control, Key.NumPad3, () => { Play_Sound(listView_mp3Files.Items[3].Text.ToString()); });
+            HotkeysManager.AddHotkey(System.Windows.Input.ModifierKeys.Control, Key.NumPad4, () => { Play_Sound(listView_mp3Files.Items[4].Text.ToString()); });
+            HotkeysManager.AddHotkey(System.Windows.Input.ModifierKeys.Control, Key.NumPad5, () => { Play_Sound(listView_mp3Files.Items[5].Text.ToString()); });
+            HotkeysManager.AddHotkey(System.Windows.Input.ModifierKeys.Control, Key.NumPad6, () => { Play_Sound(listView_mp3Files.Items[6].Text.ToString()); });
+            HotkeysManager.AddHotkey(System.Windows.Input.ModifierKeys.Control, Key.NumPad7, () => { Play_Sound(listView_mp3Files.Items[7].Text.ToString()); });
+            HotkeysManager.AddHotkey(System.Windows.Input.ModifierKeys.Control, Key.NumPad8, () => { Play_Sound(listView_mp3Files.Items[8].Text.ToString()); });
+            HotkeysManager.AddHotkey(System.Windows.Input.ModifierKeys.None, Key.NumPad9, () => { Play_Sound(listView_mp3Files.Items[9].Text.ToString()); });
+            list_device_audio();
+            list_mp3_files();
+
+        }
+        private void Play_Sound(string url)
+        {
+            if (outputDevice == null)
+            {
+                outputDevice = new WaveOutEvent() { DeviceNumber = CB_list_devices.SelectedIndex - 1};
+                outputDevice.PlaybackStopped += OnPlaybackStopped;
+            }
+            if (audioFile == null)
+            {
+                audioFile = new AudioFileReader(url.ToString());
+                outputDevice.Init(audioFile);
+            }
+
+            outputDevice.Volume = 0.5f;
+            outputDevice.Play();
+        }
+    
+        private void stop()
+        {
+
+            outputDevice?.Stop();
+        }
+        private void OnPlaybackStopped(object sender, StoppedEventArgs args)
+        {
+            outputDevice.Dispose();
+            outputDevice = null;
+            audioFile.Dispose();
+            audioFile = null;
+        }
+
+
+    }
+}
